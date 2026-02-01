a463ae820c0ca68abc5f648c1aeec411083dfb0a
Korawit Rungthampaisan <201436386+pxkrwit@users.noreply.github.com>
2026-02-01 10:16:33 +0700

    add lab 4 , Hello Tailwind

---
+++ L4-my-profile-card/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import ProfileCard from './components/ProfileCard.vue';
+
+</script>
+
+<template>
+  <div class="flex items-center justify-center gap-8 min-h-screen bg-linear-to-br from-green-500 to-sky-400">
+    <ProfileCard 
+    background="./src/assets/bg.webp"
+    profileimg="./src/assets/my-profile.png"
+    name="Korawit Rungthampaisan"
+    subheading="Student, 20, BKK"
+    ig="https://www.instagram.com/px_krwit/"
+    facebook="https://www.facebook.com/korawit.rungthampaisan/"
+    tiktok="https://www.tiktok.com/@polomoume"
+    followers="1M"
+    following="0"
+    projects="30M"
+    />
+
+    <ProfileCard 
+    background="./src/assets/fait-bg.jpg"
+    profileimg="./src/assets/fiat.png"
+    name="Borworn Kulthumyotin"
+    subheading="Student, 20, BKK"
+    ig="https://www.instagram.com/fiatkul/"
+    facebook="https://www.facebook.com/borworn.kulthumyotin"
+    tiktok="https://www.tiktok.com/@polomoume"
+    followers="100K"
+    following="100"
+    projects="30K"
+    />
+
+    <ProfileCard 
+    background="./src/assets/euro-bg.jpg"
+    profileimg="./src/assets/euro.png"
+    name="Sahaphap Yaisun"
+    subheading="Student, 21, BKK"
+    ig="https://www.instagram.com/sahapapeuro/"
+    facebook="https://www.facebook.com/sahapap.yaisun.3"
+    tiktok="https://www.tiktok.com/@sahapap.0"
+    followers="10K"
+    following="2.5k"
+    projects="3K"
+    />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ L4-my-profile-card/src/components/ProfileCard.vue
@@ -0,0 +1,82 @@
+<script setup>
+
+import facebookLogo from '../assets/Facebook-Logo.png'
+import igLogo from '../assets/ig-icon.webp'
+import tiktokLogo from '../assets/tiktok-logo.avif'
+
+defineProps([
+    "profileimg",
+    "background",
+    "name",
+    "subheading",
+    "ig",
+    "facebook",
+    "tiktok",
+    "followers",
+    "following",
+    "projects"
+])
+
+</script>
+
+<template>
+    <!-- Card -->
+     <div class="bg-white shadow-lg rounded-2xl min[320px] overflow-hidden text-center transition-all duration-300 ease-out hover:shadow-2xl hover:translate-y-5">
+        <!-- image header / profile picture -->
+         <div class="relative min-h-32 bg-cover bg-center" :style="'background-image: url(' + background + ')'">
+            <img :src="profileimg" alt="Profile Image" class="inile-block w-32 h-32 rounded-full object-center object-cover border-6 border-white absolute top-full left-1/2 transform -translate-1/2">
+         </div>
+
+         <!-- Sunheading -->
+         <div class="px-6 pt-20 pb-6">
+            <h2 class="font-bold text-gray-800 text-2xl mb-2" >{{ name }}</h2>
+
+            <h3 class="text-gray-600 text-lg">{{ subheading }}</h3>
+         </div>
+
+         <!-- Social media -->
+          <div class="px-5 pb-5 pl-10  pt-5 pr-10 flex align-middle justify-between border-t border-gray-200">
+            <div class="w-20">
+               <a :href="ig" target="_blank">
+                  <img :src="igLogo" alt="Instagram" class="inline-block w-14 h-14 rounded-full ">
+               </a>
+               <p>Instragram</p>
+            </div>
+            <div class="w-20">
+               <a :href="facebook" target="_blank" >
+                  <img :src="facebookLogo" alt="facebook" class="inline-block w-14 h-14 rounded-full">
+               </a>
+               <p>Facebook</p>
+            </div>
+            <div class="w-20">
+               <a :href="tiktok" target="_blank">
+                  <img :src="tiktokLogo" alt="tiktok" class="inline-block w-14 h-14 rounded-full">
+               </a>
+               <p>Tiktok</p>
+            </div>
+          </div>
+
+         <!-- Stats -->
+          <div class="p-6 border-t border-gray-200 flex item-center justify-center gap-6">
+            <!-- Followers -->
+             <div>
+                <h4 class="text-gray-600 text-sm">Followers</h4>
+                <p class="font-bold text-gray-800 text-2xl">{{ followers }}</p>
+             </div>
+
+             <!-- Follower -->
+             <div>
+                <h4 class="text-gray-600 text-sm">Followerings</h4>
+                <p class="font-bold text-gray-800 text-2xl">{{ following }}</p>
+             </div>
+
+             <!-- Project -->
+                <div>
+                   <h4 class="text-gray-600 text-sm">Projects</h4>
+                   <p class="font-bold text-gray-800 text-2xl">{{ projects }}</p>
+                </div>
+          </div>
+     </div>
+</template>
+
+<style scoped></style>
